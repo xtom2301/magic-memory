f97049106f8abd1cdc1c2b88db2c274eb61986f6
tmartin <xtom2301@gmail.com>
2022-01-29 14:16:45 +0100

    flipping cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import SingleCard from './components/SingleCard';
 
 const cardImages = [
-  { src: '/img/helmet-1.png' },
-  { src: '/img/potion-1.png' },
-  { src: '/img/ring-1.png' },
-  { src: '/img/scroll-1.png' },
-  { src: '/img/shield-1.png' },
-  { src: '/img/sword-1.png' },
+  { src: '/img/helmet-1.png', matched: false },
+  { src: '/img/potion-1.png', matched: false },
+  { src: '/img/ring-1.png', matched: false },
+  { src: '/img/scroll-1.png', matched: false },
+  { src: '/img/shield-1.png', matched: false },
+  { src: '/img/sword-1.png', matched: false },
 ];
 
 const App = () => {
@@ -30,23 +30,30 @@ const App = () => {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
-  useEffect(() => {
-    if (choiceOne && choiceTwo) {
-      if (choiceOne.src === choiceTwo.src) {
-        console.log('its a match');
-        resetTurn();
-      } else {
-        console.log('no match');
-        resetTurn();
-      }
-    }
-  }, [choiceOne, choiceTwo]);
-
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
-    setTurns((prevTurns) => prevTurns + 1);
+    setTurns((prevTurn) => prevTurn + 1);
   };
+
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
 
   return (
     <div className='App'>
@@ -54,7 +61,12 @@ const App = () => {
       <button onClick={shuffle}>New Game</button>
       <div className='card-grid'>
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
+          <SingleCard
+            key={card.id}
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,19 +1,21 @@
 import './SingleCard.css';
 
-const SingleCard = ({ card, handleChoice }) => {
+const SingleCard = ({ card, handleChoice, flipped }) => {
   const handleClick = () => {
     handleChoice(card);
   };
 
   return (
     <div className='card'>
-      <img className='front' src={card.src} alt='card front' />
-      <img
-        className='back'
-        onClick={handleClick}
-        src='/img/cover.png'
-        alt='card back'
-      />
+      <div className={flipped ? 'flipped' : ''}>
+        <img className='front' src={card.src} alt='card front' />
+        <img
+          className='back'
+          onClick={handleClick}
+          src='/img/cover.png'
+          alt='card back'
+        />
+      </div>
     </div>
   );
 };
